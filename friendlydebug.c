e11d9d6a81fcd1dbc7fbd08913fc4a6aacc871bd
Will Toher <will.toher.0@gmail.com>
2017-11-07 17:52:52 -0600

    Version 1.0.0

---
+++ friendlydebug.c
@@ -0,0 +1,85 @@
+/**
+ * FriendlyDebug
+ * Version 1.0.0
+ * 2017-11-07
+ * 
+ * Copyright (c) Will Toher
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+// Standard library already has all necessary include guards
+#include <stdio.h>
+#include <string.h>
+#include "friendlydebug.h"
+
+#ifndef FDBG_MAX_NAME_LENGTH
+#define FDBG_MAX_NAME_LENGTH 64
+#define FDBG_DEFAULT_NAME "Coder"
+static char __fdbg_name[FDBG_MAX_NAME_LENGTH] = FDBG_DEFAULT_NAME;
+
+static char * __fdbg_messages[FDBG_NUMMESSAGES] = { \
+    "Keep it up",
+     "You've got this",
+     "I believe in you",
+     "Don't stop now",
+     "A journey of a thousand miles ... ",
+     "... begins with the first step",
+     "Soon, your genius will be recognized",
+     "Success is the best revenge",
+     "Lookin' good",
+     "Keep on truckin'",
+     "Don't forget how great you are",
+     "You're gonna be the very best",
+     "... like no one ever was",
+     "You'll always be the best at being you",
+     "There are people who care about you",
+     "Where there's a will, there's a way",
+     "Look how far you've come already",
+     "Mama didn't raise her no quitter",
+     "I know you can do this",
+     "Let's squash this bug",
+     "You're on fire",
+     "If at first you don't succeed ... ",
+     "... try, try again"};
+//   |----------------------------------------| 40 chars, max length
+
+#endif
+
+extern char * fdbg_nextmessage() {
+    static int message_index = 0;
+    message_index++;
+    if (message_index >= FDBG_NUMMESSAGES)
+        message_index = 0;
+    return __fdbg_messages[message_index];
+}
+
+extern char * fdbg_getname() {
+    return __fdbg_name;
+}
+
+extern void fdbg_setname(char * name) {
+    if (name == NULL || name[0] == '\0') {
+        strcpy(__fdbg_name, FDBG_DEFAULT_NAME);
+        return;
+    } else {
+        strncpy(__fdbg_name, name, FDBG_MAX_NAME_LENGTH);
+    }
+}
---
+++ friendlydebug.h
@@ -0,0 +1,74 @@
+/**
+ * FriendlyDebug
+ * Version 1.0.0
+ * 2017-11-07
+ * 
+ * Copyright (c) Will Toher
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+
+#ifndef FDBG_MACROS
+#define FDBG_NUMMESSAGES 23
+
+/* Function: fdbg_info()
+ * Print the line number, function name, and file name of the location from
+ *   which it was called to standard error
+ */
+#define fdbg_info() { \
+    fprintf(stderr, "DEBUG: At line %d, function %s, file %s\n", __LINE__, __func__, __FILE__); \
+}
+
+/* Function: fdbg_message()
+ * Print an encouraging message to standard error
+ */
+#define fdbg_message() { \
+    fprintf(stderr, "DEBUG: (At line %d, func %s) %s!\n", __LINE__, __func__, fdbg_nextmessage()); \
+}
+
+/* Function: fdbg_personal()
+ * Print a personalized encouraging message to standard error
+ */
+#define fdbg_personal() {\
+    fprintf(stderr, "DEBUG: (At line %d, func %s) %s, %s!\n", __LINE__, __func__, fdbg_nextmessage(), fdbg_getname()); \
+}
+
+#define FDBG_MACROS
+#endif
+
+/* Function: fdbg_nextmessage()
+ * Returns the next encouraging message in the array
+ */
+extern char * fdbg_nextmessage();
+
+/* Function: fdgb_getname()
+ * Returns the user's previously set name (default: "Coder")
+ */
+extern char * fdbg_getname();
+
+/* Function: fdbg_setname(char * name)
+ * Set the user's name for use in messages
+ */
+extern void fdbg_setname(char * name);
+
+/* Documentation of __LINE__ etc: */
+/* https://www.lemoda.net/c/line-file-func/ */
