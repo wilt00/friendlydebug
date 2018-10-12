8b2963ee1254a277dbfc7f49dce78c8d390f79d1
Will Toher <will.toher.0@gmail.com>
2018-10-12 14:45:48 -0500

    Add beginnings of tests

---
+++ makefile
@@ -0,0 +1,3 @@
+default:
+	gcc -Wall -Wextra test.c friendlydebug.c -o test
+	./test
---
+++ test.c
@@ -0,0 +1,6 @@
+#include "./friendlydebug.h"
+
+int main(/*int argc, char **argv*/) {
+    fdbg_message();
+    return 0;
+}
